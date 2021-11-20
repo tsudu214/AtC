b5b8b437b8b7040d5f8759bcbc9a8a40619fe7b0
tsudu214 <tsudu214@gmail.com>
2021-11-21 02:45:23 +0900

    fix ABC227 C

--- ABC/c227.cpp
+++ ABC/c227.cpp
@@ -21,56 +21,7 @@ using namespace std;
 
 using ll = long long;
 
-ll search_a(ll n)
-{
-    ll low = 1;
-    ll high = n;
-    ll mid = (1+n)/2;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (mid*mid*mid > n) {
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
-    }
-    return mid;
-}
 
-ll search_b(ll a, ll n)
-{
-    ll low = a;
-    ll high = n;
-    ll mid = (1+n)/2;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (a*mid*mid > n) {
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
-    }
-    return mid;
-}
-
-ll search(ll a, ll b, ll n)
-{
-    ll low = b;
-    ll high = n;
-    ll mid = (b+n)/2;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (a*b*mid > n) {
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
-    }
-    return mid;
-}
 
 int main()
 {
@@ -79,24 +30,10 @@ int main()
 
     ll ans = 0;
 
-    ll am = search_a(n);
+    for (ll a = 1; a*a*a <= n; a++) {
 
-    for (ll a = 1; a <= am; a++) {
-        if (a*a*a > n) break;
-
-        ll bm = search_b(a, n);
-        for (ll b = a; b <= bm; b++) {
-            if (a*b*b > n) break;
-
-            ll m = search(a, b, n);
-            if (a*b*m > n) {
-                m--;
-            }
-            if (m >= b) {
-                ans += (m - b + 1);
-            } else {
-                break;
-            }
+        for (ll b = a; b*b <= n/a; b++) {
+            ans += (n/b/a - b + 1);
         }
     } 
 
